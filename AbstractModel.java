bf20a2eaf26f1ffa787e7f9c718f18a37118b2dd
konstantinos1952 <stardust20241952@gmail.com>
2024-07-17 19:00:15 +0300

    Add files via upload

---
+++ AbstractModel.java
@@ -0,0 +1,20 @@
+package loop;
+
+/**
+ * <p>Title: Gravity anomally calculations</p>
+ * <p>Description: Classes and operations for calculating gravity</p>
+ * <p>Copyright: Copyright (c) 2006</p>
+ * <p>Company: C.P.Anastasiades Ph.D</p>
+ * @author Costas
+ * @version 1.0
+ */
+
+public class AbstractModel {
+  public AbstractModel() {
+
+
+
+
+  }
+
+}
